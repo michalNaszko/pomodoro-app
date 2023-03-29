53c0cb6aae5e1f46384c5e68b2cb45ef38041017
michalNaszko <michal.naszko@gmail.com>
2023-03-29 10:09:24 +0200

    Added missed </a>

--- register.php
+++ register.php
@@ -4,7 +4,7 @@
     </head>
     <body>
         <h2>Registration Page</h2>
-        <a href="index.php">Click here to go back<br/><br/>
+        <a href="index.php">Click here to go back</a><br/><br/>
         <form action="register.php" method="POST">
            Enter Username: <input type="text" 
            name="username" required="required" /> <br/>
@@ -18,10 +18,10 @@
 <?php include 'dbCon.php';{
 
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $username = mysql_real_escape_string($_POST['username']);
-        $password = mysql_real_escape_string($_POST['password']);
-
         $conn = Connection::getConnection();
+
+        $username = $conn->quote($_POST['username']);
+        $password = $conn->quote($_POST['password']);
         
         $sql = 'INSERT INTO Users (id, login, password) VALUES (NULL, $username, $password)';
         $conn->query($sql);
