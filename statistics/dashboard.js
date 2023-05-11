c8c9a47143d092494969b8ae32a10b0d2430baae
Michal <michal.naszko@gmail.com>
2023-05-11 23:58:50 +0200

    Configured tooltips's label

--- statistics/dashboard.js
+++ statistics/dashboard.js
@@ -1,6 +1,22 @@
 /* globals Chart:false, feather:false */
 
 (function () {
+  function decycle(obj, stack = []) {
+    if (!obj || typeof obj !== 'object')
+        return obj;
+    
+    if (stack.includes(obj))
+        return null;
+
+    let s = stack.concat([obj]);
+
+    return Array.isArray(obj)
+        ? obj.map(x => decycle(x, s))
+        : Object.fromEntries(
+            Object.entries(obj)
+                .map(([k, v]) => [k, decycle(v, s)]));
+}
+
   var labels_array = [
     'Monday',
     'Tuesday',
@@ -50,52 +66,64 @@
 
   console.log("After HTTP request!\n");
 
-  function drawGraph(data) {
+  function drawGraph(data_in) {
     'use strict'
     feather.replace({ 'aria-hidden': 'true' })
-    labels_array = data.map(label => label.Date);
-    data_array = data.map( function (item) {
-        var time_array = item.Time.split(":");
-        return parseInt(time_array[0]) * 3600 + parseInt(time_array[1]) * 60 + parseInt(time_array[2]);
-      }
+    labels_array = data_in.map(label => label.Date);
+    data_array = data_in.map(function (item) {
+      var time_array = item.Time.split(":");
+      return parseInt(time_array[0]) * 3600 + parseInt(time_array[1]) * 60 + parseInt(time_array[2]);
+    }
     )
     console.log(data_array);
 
     // Graphs
     var ctx = document.getElementById('myChart')
     // eslint-disable-next-line no-unused-vars
-    var myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: labels_array,
-        datasets: [{
-          data: data_array,
-          lineTension: 0,
-          backgroundColor: 'transparent',
-          borderColor: '#007bff',
-          borderWidth: 4,
-          pointBackgroundColor: '#007bff'
-        }]
+    var data = {
+      labels: labels_array,
+      datasets: [{
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'red',
+
+        data: data_array
+      }]
+    };
+    var options = {
+      scales: {
+        y: {
+          min: 0,
+          ticks: {
+            callback: function (val, index) {
+              console.log("ticks callback");
+              return new Date(val * 1000).toISOString().substring(11, 16);
+            },
+            stepSize: (function () {
+              
+              var maxAxisValue = Math.max(...data_array) * 0.9; // add 10%
+              var step = Math.ceil((Math.ceil(maxAxisValue / 1800) / 10)) * 1800;
+              console.log("step " +step);
+              return step < 1800 ? 1800 : step;
+            })(),
+          }
+        }
       },
-      options: {
-      //   scales: {
-      //     yAxes: [{
-      //       ticks: {
-      //         beginAtZero: false
-      //       }
-      //     }]
-      //   },
-      //   legend: {
-      //     display: false
-      //   },
-        scaleOverride: true,
-        scaleLabel: function(valuePayload) {
-          console.log("#####################In scaleLabel###################");
-          console.log(new Date(valuePayload.value  * 1000).toISOString());
-         return new Date(valuePayload.value  * 1000).toISOString().substr(12, 7);
-       },
+      plugins: {
+          tooltip: {
+              callbacks: {
+                  label: function(context) {
+                      return new Date(context.parsed.y * 1000).toISOString().substring(11, 16);
+                  }
+              }
+          }
       }
-    })
+    };
+    var myNewChart = new Chart(ctx, {
+      type: "line",
+      data: data,
+      options: options,
+    });
   }
 
 })()
