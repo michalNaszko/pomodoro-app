dd514bef5db375b28e6ba9b6d5051150e835a28a
Michal <michal.naszko@gmail.com>
2023-07-02 21:10:08 +0200

    Romeved redundant code

--- php/src/statistics/statisticsModal.php
+++ php/src/statistics/statisticsModal.php
@@ -1,12 +1,6 @@
 <div class="modal-header border-bottom-0">
-  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Statistics</h1>
+  <div class="d-flex container-fluid justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <div class="btn-toolbar">
-
-      <div class="btn-group me-2">
-        <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
-      </div>
-
       <div class="btn-block dropdown me-2">
         <div class="activity-list-selected btn btn-sm btn-outline-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
           <span data-feather="activity"></span>
@@ -27,8 +21,9 @@
           <li><a class="dropdown-item" href="#">Last month</a></li>
         </ul>
       </div>
-
     </div>
+
+    <button type="button" class="ml-auto btn-close" aria-label="Close"></button>
   </div>
 </div>
 
@@ -37,32 +32,31 @@
 </div>
 
 <script>
-  if (typeof period === 'undefined') {
-    $(".activity-list li a").click(function() {
-      var activity = $(this).text().trim();
-      let period = $(".time-list-selected:first-child").text().trim();
-      var activity_type = activity == "Focus time" ? "activity" : "coffee";
-      $(this).html($(".activity-list-selected:first-child").text());
-      $(".activity-list-selected:first-child").html(' <span data-feather="' + activity_type + '"></span> ' + activity);
-      drawDashBoard(period, activity);
-      feather.replace();
-    });
+  $(".btn-close").click(function() {
+    $("#loginStat").modal('hide');
+  });
 
-    $(".time-list li a").click(function() {
-      let period = $(this).text().trim();
-      let activity = $(".activity-list-selected:first-child").text().trim();
-      $(this).html($(".time-list-selected:first-child").text());
-      $(".time-list-selected:first-child").html('<span data-feather="calendar"></span>' + period);
-      drawDashBoard(period, activity);
-      feather.replace();
-    });
-
+  $(".activity-list li a").click(function() {
+    var activity = $(this).text().trim();
+    let period = $(".time-list-selected:first-child").text().trim();
+    var activity_type = activity == "Focus time" ? "activity" : "coffee";
+    $(this).html($(".activity-list-selected:first-child").text());
+    $(".activity-list-selected:first-child").html(' <span data-feather="' + activity_type + '"></span> ' + activity);
+    drawDashBoard(period, activity);
     feather.replace();
-    var period = $(".time-list-selected:first-child").text().trim();
-    var activity = $(".activity-list-selected:first-child").text().trim();
+  });
+
+  $(".time-list li a").click(function() {
+    let period = $(this).text().trim();
+    let activity = $(".activity-list-selected:first-child").text().trim();
+    $(this).html($(".time-list-selected:first-child").text());
+    $(".time-list-selected:first-child").html('<span data-feather="calendar"></span>' + period);
     drawDashBoard(period, activity);
-  }
-  else {
-    drawDashBoard(period, activity);
-  }
+    feather.replace();
+  });
+
+  feather.replace();
+  var period = $(".time-list-selected:first-child").text().trim();
+  var activity = $(".activity-list-selected:first-child").text().trim();
+  drawDashBoard(period, activity);
 </script>
